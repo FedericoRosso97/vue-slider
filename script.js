dd0b28ee951c6695a83b8995481a1f57d1b090d9
FedericoRosso97 <federicorosso97@gmail.com>
2023-06-05 15:47:34 +0200

    creo array con img

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const {createApp}=Vue;
 createApp({
     data(){
         return{
-    
+           imgArray:[
+            '01.webp',
+            '02.webp',
+            '03.webp',
+            '04.webp',
+            '05.webp',
+           ]
         }
     }
 }).mount('#app')
